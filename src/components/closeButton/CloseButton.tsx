d645ff06db03a463600cce37d267ec8f05a88797
Margarita Bershtainis <margo.bershtainis@gmail.com>
2024-06-10 22:33:34 +0300

    did desctop modal

--- src/components/closeButton/CloseButton.tsx
+++ src/components/closeButton/CloseButton.tsx
@@ -1,9 +1,10 @@
 import cross from './cross.svg'
+import crossDesctop from './cross-desctop.svg'
 import styles from './CloseButton.module.scss'
 
 export const CloseButton = ({classname, clickFunc} : Props) => {
     return (<button onClick={clickFunc} className={styles[classname]}>
-        <img src={cross} alt='#'/>
+        <img src={(window.innerWidth > 1019 && classname === "openWindow") ? crossDesctop : cross} alt='#'/>
     </button>)
 }
 
